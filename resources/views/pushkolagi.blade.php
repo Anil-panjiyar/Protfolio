07405734b8a8f6f4181170f461ab17714912e0ef
Anil-Panjiyar <anilpanjiyar456@gmail.com>
2023-02-17 16:52:21 +0545

    change

--- resources/views/pushkolagi.blade.php
+++ resources/views/pushkolagi.blade.php
@@ -2,12 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=m, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-    sdmvdnkboenbknerj
-
+   <h1>knzfbokdf bf</h1>
 </body>
 </html>
